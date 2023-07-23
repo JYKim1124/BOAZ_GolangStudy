fdae0b156bea3b46ce81c0b14740c80a7fe4a2cd
Juyeon1124 <Johnszone@ewhain.net>
2023-07-23 23:32:46 +0900

    feat. CH2 변수와 상수

---
+++ Ch2. Constants and Variables/fmt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 int = 1
+	var num2 int = 2
+
+	fmt.Print("Hello golang!", num1, num2, "\n")
+	fmt.Println("Hello golang!", num1, num2)
+	fmt.Printf("num1의 값은 : %d num2의 값은 : %d\n", num1, num2)
+}
